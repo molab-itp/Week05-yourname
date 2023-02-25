d68a42aa435b47cdf3715bb3f7e156d59cb56cee
John Henry Thompson <jht1493@gmail.com>
2023-02-24 23:10:56 -0500

    init

---
+++ Week05-demo/Week05-demo/Week05_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week05_demoApp.swift
+//  Week05-demo
+//
+//  Created by jht2 on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Week05_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
